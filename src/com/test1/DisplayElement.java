05b3ad9d8900c5c42f5ead840bde1c1946784558
houjiaxin-aa <2877464706@qq.com>
2020-05-18 17:36:25 +0800

    Observer

---
+++ src/com/test1/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.test1;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/com/test1/WeatherData.java
@@ -0,0 +1,40 @@
+package com.test1;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class WeatherData extends Observable{
+    private double temperature;
+    private double humidity;
+    private double pressure;
+
+    public WeatherData() {}
+
+    /*向观察者发送数据*/
+    public void measurementsChanged() {
+        setChanged();		//向观察者发送数据之前必须先调用这个
+        notifyObservers();	//个人认为notifyOberservers()会自动调用观察者的 update()方法
+    }
+
+    /*从数据源收到数据后，是先更新自己的数据，再把数据发给观察者*/
+    public void setMeasurements(double temp,double humidity,double pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+    /*留出接口给观察者获取数据*/
+    public double getTemperature() {
+        return temperature;
+    }
+
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+
+    public double getPressure() {
+        return pressure;
+    }
+}
